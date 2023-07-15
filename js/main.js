039918daab35c10da8535ca598b95a570cae1949
LotusInvest <139122814+LotusInvest@users.noreply.github.com>
2023-07-16 03:22:28 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -107,6 +107,14 @@ var markers = [
           "226m2",
           "Tashkent, Uzbekistan"
       ),
+    createRentalH({
+            lat: 36.85349
+            lng: 30.62144
+        }, 
+        "https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg",
+        "89m2",
+        "Antalya"
+    ),
     // Add more markers as needed
 ];
 
